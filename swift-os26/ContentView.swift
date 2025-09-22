692722428fd1918d94497baf66f335c3fd60877c
Jan Senderek <moopoints@gmail.com>
2025-09-22 10:41:22 +0200

    Initial Commit

---
+++ swift-os26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-os26
+//
+//  Created by Jan Senderek on 9/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-os26/swift_os26App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_os26App.swift
+//  swift-os26
+//
+//  Created by Jan Senderek on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_os26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
